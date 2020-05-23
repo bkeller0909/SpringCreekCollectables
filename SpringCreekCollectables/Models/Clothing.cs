a8bd74bec55bd911cfb9aab04229271fc1bbe8f8
Brandon Keller <bkeller2@ncstudents.niagaracollege.ca>
2020-05-23 17:35:29 -0400

    Add project files.

---
+++ SpringCreekCollectables/Models/Clothing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpringCreekCollectables.Models
+{
+    public class Clothing
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public string Size { get; set; }
+        public string Brand { get; set; }
+        public string Colour { get; set; }
+        public string Description { get; set; }
+    }
+}
